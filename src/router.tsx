7b1222f4137384732f759991d4795a5bafe3fbb1
oscar2697 <lindooscar635@gmail.com>
2025-03-30 00:19:28 -0500

    Implement lazy loading for IndexPage and FavoritesPage with Suspense

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,33 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import IndexPage from "./views/IndexPage"
-import FavoritesPage from "./views/FavoritesPage"
 import Layout from "./layouts/Layout"
+import { lazy, Suspense } from "react"
+
+const IndexPage = lazy(() => import('./views/IndexPage'))
+const FavoritesPage = lazy(() => import('./views/FavoritesPage'))
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<Layout />}>
-                    <Route path='/' element={<IndexPage />} index />
-                    <Route path='/favorites' element={<FavoritesPage />} />
+                    <Route 
+                        path='/' 
+                        element={
+                            <Suspense fallback='Loading...'>
+                                <IndexPage />
+                            </Suspense>
+                        } 
+                        index 
+                    />
+
+                    <Route
+                        path='/favorites'
+                        element={
+                            <Suspense fallback='Loading...'>
+                                <FavoritesPage />
+                            </Suspense>
+                        }
+                    />
                 </Route>
             </Routes>
         </BrowserRouter>
